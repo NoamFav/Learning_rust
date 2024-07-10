4e3575a2535fdd444a6abfafa4f100461b66f79b
noamfavier <n.favier@student.maastrichtuniversity.nl>
2024-07-10 18:31:48 +0200

    website

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::str::FromStr;
+use actix_web::{web, App, HttpResponse, HttpServer, HttpRequest};
+use serde::Deserialize;
 
 mod person_mood;
 mod guess;
@@ -17,11 +19,7 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
     n
 }
 
-fn main() {
-
-    person_mood::main();
-    guess::guess();
-
+fn second() {
     let mut numbers : Vec<u64> = Vec::new();
 
     for arg in env::args().skip(1) {
@@ -40,6 +38,55 @@ fn main() {
     }
 
     println!("The greatest common divisor of {:?} is {}", numbers, d);
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
+    })
+        .bind("127.0.0.1:3000")?
+        .run()
+        .await
+}
+
+async fn get_index(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(
+            r#"
+                <title>GCD Calculator</title>
+                <form action="/gcd" method="post">
+                <input type="text" name="n"/>
+                <input type="text" name="m"/>
+                <button type="submit">Compute GCD</button>
+            </form>
+            "#
+        )
+}
+
+async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring");
+    }
+    let response = format!(
+        "The greatest common divisor of the numbers {} and {} is <b>{}</b>\n",
+        form.n, form.m, gcd(form.n, form.m)
+    );
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
+}
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
 }
 
 #[test]
